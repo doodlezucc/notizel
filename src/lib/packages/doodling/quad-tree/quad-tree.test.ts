4d6583e7a25290240b32fd73e4ef08cab1c04ae5
doodlezucc <electerminator@gmail.com>
2026-07-18 00:40:07 +0200

    Introduce quad tree populating algorithm

--- src/lib/packages/doodling/quad-tree/quad-tree.test.ts
+++ src/lib/packages/doodling/quad-tree/quad-tree.test.ts
@@ -1,7 +1,8 @@
-import { expect, test } from 'vitest';
-import { QuadTree } from './quad-tree';
+import type { Vector } from '$lib/data/common';
+import { expect, test, vi } from 'vitest';
+import { QuadTree, type QuadrantTuple, type Tile, type TileDataProvider } from './quad-tree';
 
-type Tile = string;
+type TileData = string;
 
 test.each([
 	[{ x: -1, y: -1 }, { x: 1, y: 1 }, ['0,0']],
@@ -13,7 +14,7 @@ test.each([
 	[{ x: 0.9, y: 0.9 }, { x: 1, y: 1 }, ['0,0']],
 	[{ x: 0, y: -2.5 }, { x: 3, y: 0.1 }, ['0,0', '2,-3']]
 ])('Arbitrary quad tree example $0 $1', (a, b, expectedTiles) => {
-	const tree = new QuadTree<Tile>();
+	const tree = new QuadTree<TileData>();
 
 	tree.addTileUnsafe(0, { x: 0, y: 0 }, '0,0');
 	tree.addTileUnsafe(0, { x: 2, y: -3 }, '2,-3');
@@ -23,7 +24,7 @@ test.each([
 });
 
 test('1-level grid', () => {
-	const tree = new QuadTree<Tile>();
+	const tree = new QuadTree<TileData>();
 
 	for (let x = -5; x <= 5; x++) {
 		for (let y = -5; y <= 5; y++) {
@@ -40,4 +41,82 @@ test('1-level grid', () => {
 			bottomRight: { x: 3.2, y: 1.9 }
 		})
 	).toEqual(['1,1', '2,1', '3,1']);
+});
+
+test('Populate 1-level grid', () => {
+	const tree = new QuadTree<TileData>();
+
+	for (let x = -1; x <= 0; x++) {
+		for (let y = -1; y <= 0; y++) {
+			tree.addTileUnsafe(0, { x, y }, `0(${x},${y})`);
+		}
+	}
+
+	const dataProvider = {
+		createTile: vi.fn((level: number, { x, y }: Vector): TileData => `${level}(${x},${y})`),
+		disposeTile: vi.fn<(tile: Tile<TileData>) => void>(),
+		subdivideTile: vi.fn(
+			({ level, position: { x, y } }: Tile<TileData>): QuadrantTuple<TileData> => [
+				`${level - 1}(${x * 2},${y * 2})`,
+				`${level - 1}(${x * 2 + 1},${y * 2})`,
+				`${level - 1}(${x * 2},${y * 2 + 1})`,
+				`${level - 1}(${x * 2 + 1},${y * 2 + 1})`
+			]
+		)
+	} satisfies TileDataProvider<TileData>;
+
+	const overlappingTiles = tree.populateTilesOverlappingRect(
+		{
+			topLeft: { x: -0.75, y: 0.6 },
+			bottomRight: { x: 1.25, y: 0.7 }
+		},
+		{
+			dataProvider: dataProvider,
+			targetLevel: -1
+		}
+	);
+
+	expect(overlappingTiles).toHaveLength(5);
+	expect(overlappingTiles).toEqual(
+		expect.arrayContaining([
+			expect.objectContaining<Tile<TileData>>({
+				data: '-1(-2,1)',
+				level: -1,
+				position: { x: -2, y: 1 }
+			}),
+			expect.objectContaining<Tile<TileData>>({
+				data: '-1(-1,1)',
+				level: -1,
+				position: { x: -1, y: 1 }
+			}),
+			expect.objectContaining<Tile<TileData>>({
+				data: '-1(0,1)',
+				level: -1,
+				position: { x: 0, y: 1 }
+			}),
+			expect.objectContaining<Tile<TileData>>({
+				data: '-1(1,1)',
+				level: -1,
+				position: { x: 1, y: 1 }
+			}),
+			expect.objectContaining<Tile<TileData>>({
+				data: '-1(2,1)',
+				level: -1,
+				position: { x: 2, y: 1 }
+			})
+		])
+	);
+
+	// Assert -1,0 and 0,0 to have been subdivided
+	expect(dataProvider.subdivideTile).toHaveBeenCalledTimes(2);
+	expect(dataProvider.subdivideTile.mock.calls[0][0].data).toEqual('0(-1,0)');
+	expect(dataProvider.subdivideTile.mock.calls[1][0].data).toEqual('0(0,0)');
+
+	// Assert -1,0 and 0,0 to have been disposed after subdividing
+	expect(dataProvider.disposeTile).toHaveBeenCalledTimes(2);
+	expect(dataProvider.disposeTile.mock.calls[0][0].data).toEqual('0(-1,0)');
+	expect(dataProvider.disposeTile.mock.calls[1][0].data).toEqual('0(0,0)');
+
+	// Assert only a single new leaf to have been created
+	expect(dataProvider.createTile).toHaveBeenCalledExactlyOnceWith(-1, { x: 2, y: 1 });
 });
--- src/lib/packages/doodling/quad-tree/quad-tree.ts
+++ src/lib/packages/doodling/quad-tree/quad-tree.ts
@@ -1,4 +1,20 @@
-import type { Vector } from '$lib/data/common';
+import { Vectors, type Vector } from '$lib/data/common';
+
+export interface TileLocation {
+	level: number;
+	/** Integer coordinates from the origin, unique in this `level`. */
+	position: Vector;
+}
+
+export interface Tile<T> extends TileLocation {
+	data: T;
+}
+
+export interface TileDataProvider<T> {
+	createTile: (level: number, position: Vector) => T;
+	subdivideTile: (tile: Tile<T>) => QuadrantTuple<T>;
+	disposeTile: (tile: Tile<T>) => void;
+}
 
 type Coord = `${number};${number}`;
 
@@ -12,8 +28,14 @@ interface LTRBRect {
 }
 
 interface PopulateOptions<T> {
-	detailLevel: number;
-	createTileData: (level: number, position: Vector) => T;
+	targetLevel: number;
+	dataProvider: TileDataProvider<T>;
+}
+
+interface PopulateContainerOptions<T> {
+	rect?: LTRBRect;
+	levelOffset: number;
+	dataProvider: TileDataProvider<T>;
 }
 
 // One arbitrary decision:
@@ -63,7 +85,88 @@ export class QuadTree<T> {
 		this.rootNodes = newRootNodes;
 	}
 
-	populateTilesOverlappingRect(rect: LTRBRect, options: PopulateOptions<T>): T[] {}
+	// TODO: In the future, this should use some sort of "diff" approach. Right now,
+	// subdividing a data tile multiple times (so, tile -> tiles inside containers inside container)
+	// requires an unnecessary intermediate dataProvider.subdivide(...) call.
+	populateTilesOverlappingRect(rect: LTRBRect, options: PopulateOptions<T>): Tile<T>[] {
+		const result: Tile<T>[] = [];
+
+		const { targetLevel, dataProvider } = options;
+
+		while (this.rootLevel < targetLevel) {
+			this.increaseRootLevel();
+		}
+
+		const rootTileSize = Math.pow(2, this.rootLevel);
+		const inverseRootTileSize = 1 / rootTileSize;
+
+		const { topLeft, bottomRight } = rect;
+		const minX = topLeft.x * inverseRootTileSize;
+		const minY = topLeft.y * inverseRootTileSize;
+		const maxX = bottomRight.x * inverseRootTileSize;
+		const maxY = bottomRight.y * inverseRootTileSize;
+
+		const minTileX = Math.floor(minX);
+		const minTileY = Math.floor(minY);
+		const maxTileX = Math.ceil(maxX);
+		const maxTileY = Math.ceil(maxY);
+
+		const isEdge = (x: number, y: number) => {
+			return y === minTileY || y === maxTileY - 1 || x === minTileX || x === maxTileX - 1;
+		};
+
+		for (let x = minTileX; x < maxTileX; x++) {
+			for (let y = minTileY; y < maxTileY; y++) {
+				const key = tileCoord(x, y);
+				let rootNode = this.rootNodes.get(key);
+
+				if (!rootNode) {
+					if (this.rootLevel === targetLevel) {
+						const newTileData = dataProvider.createTile(this.rootLevel, { x, y });
+						rootNode = new TreeTileNode(newTileData, this.rootLevel, { x, y });
+					} else {
+						rootNode = new TreeContainerNode(this.rootLevel, { x, y });
+					}
+
+					this.rootNodes.set(key, rootNode);
+				} else if (targetLevel < this.rootLevel && rootNode instanceof TreeTileNode) {
+					// Root tile node needs to be converted into a container node
+
+					const quadrantData = dataProvider.subdivideTile(rootNode);
+					const newContainerNode = rootNode.toContainerNode(quadrantData);
+
+					// Dispose tile in favor of the 4 new tiles
+					dataProvider.disposeTile(rootNode);
+
+					rootNode = newContainerNode;
+					this.rootNodes.set(key, newContainerNode);
+				}
+
+				if (rootNode instanceof TreeTileNode) {
+					result.push(rootNode);
+				} else if (rootNode instanceof TreeContainerNode) {
+					result.push(
+						...rootNode.populate({
+							dataProvider: dataProvider,
+							levelOffset: targetLevel - this.rootLevel + 1,
+
+							// Only tiles at the edges need to be "partially" populated.
+							// All fully surrounded tiles must be populated entirely, so
+							// the overlap check can be skipped.
+							rect: isEdge(x, y)
+								? {
+										topLeft: { x: minX - x, y: minY - y },
+										bottomRight: { x: maxX - x, y: maxY - y }
+									}
+								: undefined
+						})
+					);
+				}
+			}
+		}
+
+		return result;
+	}
 
 	findTilesOverlappingRect(rect: LTRBRect): T[] {
 		const result: T[] = [];
@@ -113,9 +216,8 @@ export class QuadTree<T> {
 	}
 }
 
-abstract class TreeNode<T> {
+abstract class TreeNode<T> implements TileLocation {
 	readonly level: number;
-	/** Integer coordinates from the origin, unique in this `level`. */
 	readonly position: Vector;
 
 	constructor(level: number, position: Vector) {
@@ -126,14 +228,28 @@ abstract class TreeNode<T> {
 	abstract get selfOrDescendantData(): T[];
 }
 
-type CornerTuple<T> = [topLeft?: T, topRight?: T, bottomLeft?: T, bottomRight?: T];
+export type QuadrantTuple<T> = [
+	topLeft: T | null,
+	topRight: T | null,
+	bottomLeft: T | null,
+	bottomRight: T | null
+];
+
+interface ProcessQuadrantsOptions {
+	rect?: LTRBRect;
+}
+
+interface QuadrantContext {
+	positionInContainer: Vector;
+	localRect?: LTRBRect;
+}
 
 class TreeContainerNode<T> extends TreeNode<T> {
-	private readonly children: CornerTuple<TreeNode<T>>;
+	private readonly children: QuadrantTuple<TreeNode<T>>;
 
-	constructor(level: number, position: Vector, children?: CornerTuple<TreeNode<T>>) {
+	constructor(level: number, position: Vector, children?: QuadrantTuple<TreeNode<T>>) {
 		super(level, position);
-		this.children = children ?? [undefined, undefined, undefined, undefined];
+		this.children = children ?? [null, null, null, null];
 	}
 
 	override get selfOrDescendantData(): T[] {
@@ -152,10 +268,27 @@ class TreeContainerNode<T> extends TreeNode<T> {
 		this.children[x + y * 2] = node;
 	}
 
-	private processQuadrantsOverlappingRect(
-		rect: LTRBRect,
-		processQuadrant: (node: TreeNode<T> | undefined, localRect: LTRBRect) => void
+	private processAllQuadrants(
+		processQuadrant: (node: TreeNode<T> | null, context: QuadrantContext) => TreeNode<T> | null
 	) {
+		for (let x = 0; x < 2; x++) {
+			for (let y = 0; y < 2; y++) {
+				this.children[x + y * 2] = processQuadrant(this.children[x + y * 2], {
+					positionInContainer: { x, y }
+				});
+			}
+		}
+	}
+
+	private processQuadrants(
+		processQuadrant: (node: TreeNode<T> | null, context: QuadrantContext) => TreeNode<T> | null,
+		{ rect }: ProcessQuadrantsOptions = {}
+	) {
+		if (!rect) {
+			this.processAllQuadrants(processQuadrant);
+			return;
+		}
+
 		const {
 			topLeft: { x: ax, y: ay },
 			bottomRight: { x: bx, y: by }
@@ -166,41 +299,51 @@ class TreeContainerNode<T> extends TreeNode<T> {
 		}
 
 		if (ax <= 0 && ay <= 0 && bx >= 1 && by >= 1) {
-			for (const child of this.children) {
-				processQuadrant(child, rect);
-			}
+			this.processAllQuadrants(processQuadrant);
 			return;
 		}
 
+		const computeQuadrantContext = (position: Vector): QuadrantContext => {
+			const xOffset = position.x * 0.5;
+			const yOffset = position.y * 0.5;
+			return {
+				positionInContainer: position,
+				localRect: {
+					topLeft: { x: rect.topLeft.x * 2 - xOffset, y: rect.topLeft.y * 2 - yOffset },
+					bottomRight: { x: rect.bottomRight.x * 2 - xOffset, y: rect.bottomRight.y * 2 - yOffset }
+				}
+			};
+		};
+
 		if (ax < 0.5) {
 			if (ay < 0.5) {
-				processQuadrant(this.children[0], {
-					topLeft: { x: ax * 2, y: ay * 2 },
-					bottomRight: { x: bx * 2, y: by * 2 }
-				});
+				this.children[0] = processQuadrant(
+					this.children[0],
+					computeQuadrantContext({ x: 0, y: 0 })
+				);
 			}
 
 			if (by > 0.5) {
-				processQuadrant(this.children[2], {
-					topLeft: { x: ax * 2, y: ay * 2 - 0.5 },
-					bottomRight: { x: bx * 2, y: by * 2 - 0.5 }
-				});
+				this.children[2] = processQuadrant(
+					this.children[2],
+					computeQuadrantContext({ x: 0, y: 1 })
+				);
 			}
 		}
 
 		if (bx > 0.5) {
 			if (ay < 0.5) {
-				processQuadrant(this.children[1], {
-					topLeft: { x: ax * 2 - 0.5, y: ay * 2 },
-					bottomRight: { x: bx * 2 - 0.5, y: by * 2 }
-				});
+				this.children[1] = processQuadrant(
+					this.children[1],
+					computeQuadrantContext({ x: 1, y: 0 })
+				);
 			}
 
 			if (by > 0.5) {
-				processQuadrant(this.children[3], {
-					topLeft: { x: ax * 2 - 0.5, y: ay * 2 - 0.5 },
-					bottomRight: { x: bx * 2 - 0.5, y: by * 2 - 0.5 }
-				});
+				this.children[3] = processQuadrant(
+					this.children[3],
+					computeQuadrantContext({ x: 1, y: 1 })
+				);
 			}
 		}
 	}
@@ -208,23 +351,80 @@ class TreeContainerNode<T> extends TreeNode<T> {
 	getDescendantDataInBounds(rect: LTRBRect): T[] {
 		const result: T[] = [];
 
-		const processQuadrant = (quadrant: TreeNode<T> | undefined, localRect: LTRBRect) => {
-			if (quadrant instanceof TreeTileNode) {
-				result.push(quadrant.data);
-			} else if (quadrant instanceof TreeContainerNode) {
-				quadrant.processQuadrantsOverlappingRect(localRect, processQuadrant);
-			}
-		};
-
-		this.processQuadrantsOverlappingRect(rect, processQuadrant);
+		this.processQuadrants(
+			(quadrant, context) => {
+				if (quadrant instanceof TreeTileNode) {
+					result.push(quadrant.data);
+				} else if (quadrant instanceof TreeContainerNode) {
+					if (context.localRect) {
+						result.push(...quadrant.getDescendantDataInBounds(context.localRect));
+					} else {
+						result.push(...quadrant.selfOrDescendantData);
+					}
+				}
+				return quadrant;
+			},
+			{ rect: rect }
+		);
 
 		return result;
 	}
 
-	populate(options: PopulateOptions<T>) {}
+	populate(options: PopulateContainerOptions<T>): Tile<T>[] {
+		const { dataProvider, levelOffset, rect } = options;
+		const result: Tile<T>[] = [];
+
+		this.processQuadrants(
+			(quadrant, context) => {
+				if (!quadrant) {
+					const childPosition: Vector = {
+						x: this.position.x * 2 + context.positionInContainer.x,
+						y: this.position.y * 2 + context.positionInContainer.y
+					};
+
+					if (levelOffset === 0) {
+						quadrant = new TreeTileNode<T>(
+							dataProvider.createTile(this.level - 1, childPosition),
+							this.level - 1,
+							childPosition
+						);
+					} else {
+						quadrant = new TreeContainerNode<T>(this.level - 1, childPosition);
+					}
+				}
+
+				if (quadrant instanceof TreeTileNode) {
+					if (levelOffset === 0) {
+						result.push(quadrant);
+					} else {
+						// This leaf quadrant needs to have further subdivisions
+						const replacementNode = quadrant.toContainerNode(dataProvider.subdivideTile(quadrant));
+						dataProvider.disposeTile(quadrant);
+
+						quadrant = replacementNode;
+					}
+				}
+
+				if (quadrant instanceof TreeContainerNode) {
+					result.push(
+						...quadrant.populate({
+							dataProvider: dataProvider,
+							levelOffset: levelOffset - 1,
+							rect: context.localRect
+						})
+					);
+				}
+
+				return quadrant;
+			},
+			{ rect: rect }
+		);
+
+		return result;
+	}
 }
 
-class TreeTileNode<T> extends TreeNode<T> {
+class TreeTileNode<T> extends TreeNode<T> implements Tile<T> {
 	readonly data: T;
 
 	constructor(data: T, level: number, position: Vector) {
@@ -234,5 +434,23 @@ class TreeTileNode<T> extends TreeNode<T> {
 
 	override get selfOrDescendantData(): T[] {
 		return [this.data];
+	}
+
+	toContainerNode(quadrantData: QuadrantTuple<T>): TreeContainerNode<T> {
+		const childLevel = this.level - 1;
+		const { x, y } = Vectors.scale(this.position, 2);
+
+		return new TreeContainerNode<T>(this.level, this.position, [
+			quadrantData[0] !== null ? new TreeTileNode(quadrantData[0], childLevel, { x, y }) : null,
+			quadrantData[1] !== null
+				? new TreeTileNode(quadrantData[1], childLevel, { x: x + 1, y: y })
+				: null,
+			quadrantData[2] !== null
+				? new TreeTileNode(quadrantData[2], childLevel, { x: x, y: y + 1 })
+				: null,
+			quadrantData[3] !== null
+				? new TreeTileNode(quadrantData[3], childLevel, { x: x + 1, y: y + 1 })
+				: null
+		]);
 	}
 }
